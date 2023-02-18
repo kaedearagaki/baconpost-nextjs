64fb657ec205dbc83c4ca7753bd806508bf2d03c
kaedearagaki <125759864+kaedearagaki@users.noreply.github.com>
2023-02-19 01:56:17 +0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import axios from "axios"
+import AddPost from "./components/AddPost"
+import { useQuery } from "@tanstack/react-query"
+import Post from "./components/Post"
+import { PostType } from "./types/Posts"
+
+//fetch all post
+const allPosts = async() => {
+  const response = await axios.get("/api/posts/getPosts")
+  return response.data
+}
+
+export default function Home() {
+  const {data, error, isLoading} = useQuery<PostType[]>({
+    queryFn: allPosts, 
+    queryKey: ["posts"],
+  })
+  if (error) return error
+  if (isLoading) return "Loading..."
+  
+  return (
+    <main>
+      <AddPost />
+      {data?.map((post) => (
+        <Post 
+          comment={post.Comment}
+          key={post.id} 
+          name={post.user.name} 
+          avatar={post.user.image} 
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
+    </main>
+  )
+}
+
