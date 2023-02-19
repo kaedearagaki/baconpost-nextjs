286761df7751f2b4d664def33135588471da0bb2
kaedearagaki <125759864+kaedearagaki@users.noreply.github.com>
2023-02-19 13:44:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   experimental: {
     appDir: true,
   },
   images: {
     domains: ["lh3.googleusercontent.com"]
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 }
 
